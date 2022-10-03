2cbeaf9d7c97cc4eda85312159ec4447add877f2
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-10-03 02:25:40 +0200

    Fixed XPaths and variable declarations

--- src/ScrapeTikTokComments.js
+++ src/ScrapeTikTokComments.js
@@ -1,16 +1,16 @@
 with ({copy}) (async function() {
-    var AllCommentsXPath                 = '/html/body/div[2]/div[2]/div[3]/div[2]/div[3]';
-    var level2CommentsXPath              = '/html/body/div/div[2]/div[3]/div[2]/div[3]/div/div[2]/div/a';
+    let i;
+    const AllCommentsXPath = '//*[contains(@class,\'DivCommentListContainer\')]';
+    const level2CommentsXPath = '//*[contains(@class,\'DivReplyContainer\')]';
 
-    var publisherProfileUrlXPath         = '/html/body/div/div[2]/div[3]/div[2]/div[1]/a[1]';
-    var nicknameAndTimePublishedAgoXPath = '/html/body/div/div[2]/div[3]/div[2]/div[1]/a[2]/span[2]';
+    const publisherProfileUrlXPath = '//*[contains(@class,\'StyledLink\') and contains(@class,\'tiktok\')]';
+    const nicknameAndTimePublishedAgoXPath = '//*[contains(@data-e2e,\'browser-nickname\')]';
 
-    var postUrlXPath                     = '/html/body/div/div[2]/div[3]/div[2]/div[2]/div[2]/div[2]/p';
-    var likeCountXPath                   = '/html/body/div/div[2]/div[3]/div[2]/div[2]/div[2]/div[1]/div[1]/button[1]/strong';
-    var descriptionXPath                 = '/html/body/div/div[2]/div[3]/div[2]/div[2]/div[1]';
-    var tiktokNumberOfCommentsXPath      = '/html/body/div/div[2]/div[3]/div[2]/div[2]/div[2]/div[1]/div[1]/button[2]/strong';
+    const likeCountXPath = '//*[contains(@data-e2e,\'like-count\')]';
+    const descriptionXPath = '//*[contains(@data-e2e,\'browse-video-desc\')]';
+    const tiktokNumberOfCommentsXPath = '//*[contains(@data-e2e,\'comment-count\')]';
 
-    var readMoreDivXPath                 = '/html/body/div/div[2]/div[3]/div[2]/div[3]/div/div[2]/div/p[1]/text()/..';
+    const readMoreDivXPath = '//*[contains(@data-e2e,\'view-more\')]';
 
     // more reliable than querySelector
     function getElementsByXPath(xpath, parent)
@@ -33,13 +33,14 @@ with ({copy}) (async function() {
     }
 
     function formatDate(strDate) {
+        let f;
         if (typeof strDate !== 'undefined' && strDate !== null) {
             f = strDate.split('-');
-            if (f.length == 1) {
+            if (f.length === 1) {
                 return strDate;
-            } else if (f.length == 2) {
+            } else if (f.length === 2) {
                 return f[1] + '-' + f[0] + '-' + (new Date().getFullYear());
-            } else if (f.length == 3) {
+            } else if (f.length === 3) {
                 return f[2] + '-' + f[1] + '-' + f[0];
             } else {
                 return 'Malformed Date';
@@ -50,18 +51,21 @@ with ({copy}) (async function() {
     }
 
     function csvFromComment(comment) {
-        nickname = getElementsByXPath('./div[1]/a', comment)[0].outerText;
-        user = getElementsByXPath('./a', comment)[0]['href'].split('?')[0].split('/')[3].slice(1);
-        commentText = getElementsByXPath('./div[1]/p', comment)[0].outerText;
-        timeCommentedAgo = formatDate(getElementsByXPath('./div[1]/p[2]/span', comment)[0].outerText);
-        commentLikesCount = getElementsByXPath('./div[2]', comment)[0].outerText;
-        pic = getElementsByXPath('./a/span/img', comment)[0]['src'];
+        let nickname = getElementsByXPath('./div[1]/a', comment)[0].outerText;
+        let user = getElementsByXPath('./a', comment)[0]['href'].split('?')[0].split('/')[3].slice(1);
+        let commentText = getElementsByXPath('./div[1]/p', comment)[0].outerText;
+        let timeCommentedAgo = formatDate(getElementsByXPath('./div[1]/p[2]/span', comment)[0].outerText);
+        let commentLikesCount = getElementsByXPath('./div[2]', comment)[0].outerText;
+        let pic = getElementsByXPath('./a/span/img', comment)[0]['src'];
         return quoteString(nickname) + ',' + quoteString(user) + ',' + 'https://example.com' + ',' + quoteString(commentText) + ',' + timeCommentedAgo + ',' + commentLikesCount + ',' + quoteString(pic);
     }
 
     // Loading 1st level comments
-    var loadingCommentsBuffer = 30; // increase buffer if loading comments takes long and the loop break too soon
-    var numOfcommentsBeforeScroll = getAllComments().length;
+    let loadingCommentsBuffer = 30; // increase buffer if loading comments takes long and the loop break too soon
+    let numOfCommentsBeforeScroll = getAllComments().length;
+    let allComments;
+    let lastComment;
+    let numOfCommentsAfterScroll;
     while (loadingCommentsBuffer > 0) {
 
         allComments = getAllComments();
@@ -69,28 +73,30 @@ with ({copy}) (async function() {
         // Scroll last element into view = scrolling to bottom of comment page
         lastComment.scrollIntoView(false);
 
-        numOfcommentsAftScroll = getAllComments().length;
+        numOfCommentsAfterScroll = getAllComments().length;
 
         // If number of comments doesn't change after 15 iterations, break the loop.
-        if (numOfcommentsAftScroll !== numOfcommentsBeforeScroll) {
+        if (numOfCommentsAfterScroll !== numOfCommentsBeforeScroll) {
             loadingCommentsBuffer = 15;
         } else {
             loadingCommentsBuffer--;
-        };
-        numOfcommentsBeforeScroll = numOfcommentsAftScroll;
-        console.log('Loading 1st level comment number ' + numOfcommentsAftScroll);
+        }
+
+        numOfCommentsBeforeScroll = numOfCommentsAfterScroll;
+        console.log('Loading 1st level comment number ' + numOfCommentsAfterScroll);
 
         // Wait 0.3 seconds.
         await new Promise(r => setTimeout(r, 300));
     }
-    console.log('Openend all 1st level comments');
+    console.log('Opened all 1st level comments');
 
 
     // Loading 2nd level comments
     loadingCommentsBuffer = 5; // increase buffer if loading comments takes long and the loop break too soon
+    let readMoreDivs;
     while (loadingCommentsBuffer > 0) {
         readMoreDivs = getElementsByXPath(readMoreDivXPath);
-        for (var i = 0; i < readMoreDivs.length; i++) {
+        for (i = 0; i < readMoreDivs.length; i++) {
             readMoreDivs[i].click()
         }
 
@@ -102,21 +108,21 @@ with ({copy}) (async function() {
         }
         console.log('Buffer ' + loadingCommentsBuffer);
     }
-    console.log('Openened all 2nd level comments');
+    console.log('Opened all 2nd level comments');
 
 
     // Reading all comments, extracting and converting the data to csv
-    var comments = getAllComments();
+    const comments = getAllComments();
 
-    var publisherProfileUrl = getElementsByXPath(publisherProfileUrlXPath)[0]['href'].split('?')[0];
-    var nicknameAndPublishedAgoTime = getElementsByXPath(nicknameAndTimePublishedAgoXPath)[0].outerText.replaceAll('\n', ' ').split(' · ');
-    var level2commentsLength = getElementsByXPath(level2CommentsXPath).length;
+    const publisherProfileUrl = getElementsByXPath(publisherProfileUrlXPath)[2]['href'];
+    const nicknameAndPublishedAgoTime = getElementsByXPath(nicknameAndTimePublishedAgoXPath)[0].outerText.replaceAll('\n', ' ').split(' · ');
+    const level2commentsLength = getElementsByXPath(level2CommentsXPath).length;
 
-    var commentNumberDifference = Math.abs(getElementsByXPath(tiktokNumberOfCommentsXPath)[0].outerText - (comments.length + level2commentsLength))
+    const commentNumberDifference = Math.abs(getElementsByXPath(tiktokNumberOfCommentsXPath)[0].outerText - (comments.length + level2commentsLength));
 
 
-    var csv = 'Now,' + Date() + '\n';
-    csv += 'Post URL,' + getElementsByXPath(postUrlXPath)[0].outerText.split('?')[0] + '\n';
+    let csv = 'Now,' + Date() + '\n';
+    csv += 'Post URL,' + window.location.href.split('?')[0] + '\n';
     csv += 'Publisher Nickname,' + nicknameAndPublishedAgoTime[0] + '\n';
     csv += 'Publisher URL,' + publisherProfileUrl + '\n';
     csv += 'Publisher @,' + publisherProfileUrl.split('/')[3].slice(1) + '\n';
@@ -129,26 +135,30 @@ with ({copy}) (async function() {
     csv += "Total Comments (which TikTok tells you; it's too high most of the time when dealing with many comments OR way too low because TikTok limits the number of comments to prevent scraping)," + getElementsByXPath(tiktokNumberOfCommentsXPath)[0].outerText + '\n';
     csv += "Difference," + commentNumberDifference + '\n';
     csv += 'Comment Number (ID),Nickname,User @,User URL,Comment Text,Time,Likes,Profile Picture URL,Is 2nd Level Comment,Replied To,Number of replies\n';
-    var count = 1;
+    let count = 1;
 
-    for (var i = 0; i < comments.length; i++) {
+    let level1comment;
+    let more;
+    let numberOfReplies;
+    let repliedTo;
+    for (i = 0; i < comments.length; i++) {
         level1comment = comments[i].children[0]
         more = comments[i].children[1]
         if (typeof more !== 'undefined') {
-            more = [...more.children].slice(0,-1);
+            more = [...more.children].slice(0, -1);
             numberOfReplies = more.length;
         } else {
             numberOfReplies = 0;
         }
         csv += count + ',' + csvFromComment(level1comment) + ',No,---,' + numberOfReplies + '\n';
         repliedTo = getElementsByXPath('./a', level1comment)[0]['href'].split('?')[0].split('/')[3].slice(1);
-        for (j = 0; j < numberOfReplies; j++) {
+        for (let j = 0; j < numberOfReplies; j++) {
             count++;
             csv += count + ',' + csvFromComment(more[j]) + ',Yes,' + repliedTo + ',---\n';
         }
         count++;
     }
-    var apparentCommentNumber = getElementsByXPath(tiktokNumberOfCommentsXPath)[0].outerText;
+    const apparentCommentNumber = getElementsByXPath(tiktokNumberOfCommentsXPath)[0].outerText;
     console.log('Number of magically missing comments (not rendered in the comment section): ' + (apparentCommentNumber - count + 1) + ' (you have ' + (count - 1) + ' of ' + apparentCommentNumber + ')');
     console.log('CSV copied to clipboard!');
 
